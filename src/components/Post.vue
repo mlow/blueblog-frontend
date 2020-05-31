9e73193532e07cc0704a2c82713f54023be0e879
Matt Low <mattjlow94@gmail.com>
2020-05-31 18:53:23 +0400

    Better date formatting

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -2,7 +2,7 @@
   <article>
     <header>
       <h2 class="title">{{ title }}</h2>
-      <span class="publish_date">{{ publish_date.toLocaleDateString() }}</span>
+      <span class="publish_date">{{ publish_date_formatted }}</span>
     </header>
     <div class="content">
       <VueMarkdown :source="content"></VueMarkdown>
@@ -12,12 +12,18 @@
 
 <script>
 import VueMarkdown from "vue-markdown";
+import date from "date-and-time";
 
 export default {
   props: {
     title: String,
     content: String,
     publish_date: Date
+  },
+  computed: {
+    publish_date_formatted() {
+      return date.format(this.publish_date, "ddd, MMM D, YYYY");
+    }
   },
   components: {
     VueMarkdown
